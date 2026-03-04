5a10462fae516828013170cc401c961b5431fc1a
Yuval Galili Kedem <yuvalgk@Yuvals-MacBook-Air.local>
2026-03-05 00:32:17 +0200

    Docker Postgres Set Up Try

--- postgres.py
+++ postgres.py
@@ -1,13 +1,34 @@
-from sqlalchemy import create_engine
-from sqlalchemy import text
-from faker import Faker
-import logging, time
+import uuid
+import enum
+
+from sqlalchemy import String, Enum, DateTime, create_engine
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column
+
+import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 LOGGER = logging.getLogger(__name__)
 
+class Status(enum.Enum):
+    CREATED = 'created',
+    DELETED = 'deleted'
+
+
+Base = declarative_base()
+
+class Deployments(Base):
+    __tablename__ = 'deployments'
+    id: Mapped[uuid.UUID] = mapped_column(default=uuid.uuid4(), primary_key=True)
+    db_name: Mapped[str] = mapped_column(String(30))
+    status: Mapped[Enum] = mapped_column(Enum(Status))
+    username: Mapped[str] = mapped_column(String(30))
+    creation_time: Mapped[DateTime] = mapped_column(DateTime)
+
+
 def get_db_engine():
-    return create_engine('postgresql://{}:{}@{}/{}'.format('postgres', 'postgres', 'postgres:5432', 'a_mongo_ass_db'))
+    engine = create_engine('postgresql+psycopg2://{}:{}@{}/{}'.format('postgres', 'postgres', 'localhost:5432', 'a_mongo_ass_db'))
+    Base.metadata.create_all(engine)
+    return engine
 
 while True:
     try:
